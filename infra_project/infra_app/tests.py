8c4c374c593125acbf555f4fc87dd9291f764be4
Daria Riazanova <d.ryazanova07@gmail.com>
2021-06-28 23:29:02 +0300

    fix pep

--- infra_project/infra_app/tests.py
+++ infra_project/infra_app/tests.py
@@ -11,7 +11,6 @@ class StaticPagesURLTests(TestCase):
         """Проверка доступности страниц."""
         response = self.guest_client.get('/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
-
         response = self.guest_client.get('/second_page/')
         self.assertEqual(response.status_code, HTTPStatus.OK)
 
@@ -19,7 +18,5 @@ class StaticPagesURLTests(TestCase):
         """Проверка контекста страниц."""
         response = self.guest_client.get('/')
         self.assertContains(response, 'У меня получилось!')
-
-
         response = self.guest_client.get('/second_page/')
         self.assertContains(response, 'А это вторая страница!')
